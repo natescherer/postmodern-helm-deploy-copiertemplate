310911fced971c93e82933fc1a8e3fb29343c575
Nate Scherer <376408+natescherer@users.noreply.github.com>
2025-08-26 02:36:11 +0000

    feat: apply parent remplate v0.5.13 (#125)
    * feat: move answers file
    
    * feat: apply template v0.5.13
    
    * feat: adjust releasing

--- template/tasks.py
+++ template/tasks.py
@@ -7,58 +7,55 @@ import json
 import os
 import shutil
 import tempfile
+import time
 from pathlib import Path
 
-import copier.vcs
 import githubkit
 import requests
 from invoke import task
 from rich import print
 
 
-@task
-def copy_template_files(c, answers_json):
+@task(optional=["vcs_ref"])
+def copy_template_files(c, src_path, vcs_ref=None):
     """Pull down an additional copy of template files."""
     print("[bold green]*** 'copy-template-files' task start ***[/bold green]")
-    answers = json.loads(answers_json)
-    source = answers["_src_path"]
-    ref = answers["_commit"]
-    source_url = copier.vcs.get_repo(source)
 
     with tempfile.TemporaryDirectory() as tmpdir:
-        if ref != "HEAD" and ref is not None:
+        if vcs_ref != "HEAD" and vcs_ref is not None:
+            time.sleep(5)
             c.run(
-                f"cd {tmpdir}; git -c advice.detachedHead=false clone -q "
-                f"--depth 1 --branch {ref} {source_url} ."
+                f"git -c advice.detachedHead=false clone --quiet "
+                f"--branch {vcs_ref} {src_path} {tmpdir}"
             )
         else:
-            c.run(
-                f"cd {tmpdir}; git -c advice.detachedHead=false clone -q "
-                f"--depth 1 {source_url} ."
-            )
+            c.run(f"git -c advice.detachedHead=false clone --quiet {src_path} {tmpdir}")
         shutil.copytree(f"{tmpdir}/template", "template", dirs_exist_ok=True)
     print("[bold green]*** 'copy-template-files' task end ***[/bold green]")
 
 
-@task
-def create_repo_github(c, answers_json):
+@task(optional=["github_org"])
+def create_repo_github(
+    c, repo_name, github_repo_description, github_repo_owner, is_public, github_org=None
+):
     """Create a GitHub repo."""
     print("[bold green]*** 'create-repo-github' task start ***[/bold green]")
-    answers = json.loads(answers_json)
     with open("token.json") as token_file:
         token = json.loads(token_file.read())["token"]
 
     print("[cyan]Authenticating to GitHub...[/cyan]")
     github = githubkit.GitHub(githubkit.TokenAuthStrategy(token))
     repo_data = {
-        "name": answers["repo_name"],
-        "description": answers["project_short_description"],
-        "homepage": answers["project_website"],
-        "private": True if answers["project_visibility"] == "Private" else False,
+        "name": repo_name,
+        "description": github_repo_description,
+        "homepage": f"https://{github_repo_owner}.github.io/{repo_name}"
+        if is_public
+        else "",
+        "private": True if not is_public else False,
     }
-    if answers["github_org"]:
+    if github_org:
         print("[cyan]Creating repo in GitHub organization...[/cyan]")
-        github.rest.repos.create_in_org(answers["github_org"], data=repo_data)
+        github.rest.repos.create_in_org(github_org, data=repo_data)
     else:
         repo_data.update({"allow_auto_merge": True, "delete_branch_on_merge": True})
         print("[cyan]Creating repo in GitHub user account...[/cyan]")
@@ -67,19 +64,18 @@ def create_repo_github(c, answers_json):
 
 
 @task
-def create_repo_azdo(c, answers_json):
+def create_repo_azdo(c, repo_name, azdo_project, azdo_org):
     """Create an Azure DevOps repo."""
     print("[bold green]*** 'create-repo-azdo' task start ***[/bold green]")
-    answers = json.loads(answers_json)
     with open("token.json") as token_file:
         token = json.loads(token_file.read())["token"]
 
-    repo_data = {"name": answers["repo_name"]}
+    repo_data = {"name": repo_name}
     headers = {"Content-Type": "application/json", "Accept": "application/json"}
-    encoded_project = answers["azdo_project"].replace(" ", "%20")
+    encoded_project = azdo_project.replace(" ", "%20")
     print("[cyan]Creating repo in Azure DevOps...[/cyan]")
     response = requests.post(
-        f"https://dev.azure.com/{answers['azdo_org']}/{encoded_project}/_apis/git/repositories?api-version=7.2-preview.1",
+        f"https://dev.azure.com/{azdo_org}/{encoded_project}/_apis/git/repositories?api-version=7.2-preview.1",
         data=json.dumps(repo_data),
         auth=("", token),
         headers=headers,
@@ -89,26 +85,28 @@ def create_repo_azdo(c, answers_json):
     print("[bold green]*** 'create-repo-azdo' task end ***[/bold green]")
 
 
-@task
-def set_repo_settings_github(c, answers_json):
+@task(optional=["github_org"])
+def set_repo_settings_github(
+    c, github_repo_owner, repo_name, is_public, github_org=None
+):
     """Set settings on a GitHub repo."""
     print("[bold green]*** 'set-repo-settings-github' task start ***[/bold green]")
-    answers = json.loads(answers_json)
     with open("token.json") as token_file:
         token = json.loads(token_file.read())["token"]
-    owner = answers.get("github_org") or answers.get("github_username")
 
     print("[cyan]Authenticating to GitHub...[/cyan]")
     github = githubkit.GitHub(githubkit.TokenAuthStrategy(token))
 
     # Auto-merge and delete branch on merge
-    if not answers["github_org"]:
+    if not github_org:
         repo_data = {"allow_auto_merge": True, "delete_branch_on_merge": True}
-        github.rest.repos.update(owner=owner, repo=answers["repo_name"], data=repo_data)
+        github.rest.repos.update(
+            owner=github_repo_owner, repo=repo_name, data=repo_data
+        )
 
     # Labels
     current_label_content = github.rest.issues.list_labels_for_repo(
-        owner=owner, repo=answers["repo_name"]
+        owner=github_repo_owner, repo=repo_name
     )
     current_labels = [x.name for x in current_label_content.parsed_data]
 
@@ -120,7 +118,7 @@ def set_repo_settings_github(c, answers_json):
         }
         print("[cyan]Creating 'awaiting pr' label...[/cyan]")
         github.rest.issues.create_label(
-            owner=owner, repo=answers["repo_name"], data=awaiting_pr_label_data
+            owner=github_repo_owner, repo=repo_name, data=awaiting_pr_label_data
         )
     else:
         print("[yellow]Label 'awaiting pr' already exists, skipping creation.[/yellow]")
@@ -132,7 +130,7 @@ def set_repo_settings_github(c, answers_json):
         }
         print("[cyan]Creating 'blocked' label...[/cyan]")
         github.rest.issues.create_label(
-            owner=owner, repo=answers["repo_name"], data=blocked_label_data
+            owner=github_repo_owner, repo=repo_name, data=blocked_label_data
         )
     else:
         print("[yellow]Label 'blocked' already exists, skipping creation.[/yellow]")
@@ -141,28 +139,27 @@ def set_repo_settings_github(c, answers_json):
     workflow_perm_data = {"can_approve_pull_request_reviews": True}
     print("[cyan]Setting Actions workflow settings...[/cyan]")
     github.rest.actions.set_github_actions_default_workflow_permissions_repository(
-        owner=owner, repo=answers["repo_name"], data=workflow_perm_data
+        owner=github_repo_owner, repo=repo_name, data=workflow_perm_data
     )
+
     print("[bold green]*** 'set-repo-settings-github' task end ***[/bold green]")
 
 
 @task
-def set_branch_protection_ruleset_github(c, answers_json):
+def set_branch_protection_ruleset_github(c, github_repo_owner, repo_name):
     """Set branch protection ruleset on a GitHub repo."""
     print(
         "[bold green]"
         "*** 'set-branch-protection-ruleset-github' task start ***"
         "[/bold green]"
     )
-    answers = json.loads(answers_json)
     with open("token.json") as token_file:
         token = json.loads(token_file.read())["token"]
-    owner = answers.get("github_org") or answers.get("github_username")
 
     print("[cyan]Authenticating to GitHub...[/cyan]")
     github = githubkit.GitHub(githubkit.TokenAuthStrategy(token))
     rulesets_content = github.rest.repos.get_repo_rulesets(
-        owner=owner, repo=answers["repo_name"]
+        owner=github_repo_owner, repo=repo_name
     )
     rulesets = [x.name for x in rulesets_content.parsed_data]
     if "default-branch-protection" not in rulesets:
@@ -189,7 +186,7 @@ def set_branch_protection_ruleset_github(c, answers_json):
         }
         print("[cyan]Creating branch protection ruleset...[/cyan]")
         github.rest.repos.create_repo_ruleset(
-            owner=owner, repo=answers["repo_name"], data=ruleset_data
+            owner=github_repo_owner, repo=repo_name, data=ruleset_data
         )
     else:
         print(
@@ -204,17 +201,24 @@ def set_branch_protection_ruleset_github(c, answers_json):
     )
 
 
-@task
-def initialize_repo_and_commit_files(c, answers_json):
+@task(optional=["github_username", "github_repo_owner", "azdo_org", "azdo_project"])
+def initialize_repo_and_commit_files(
+    c,
+    lifecycle,
+    developer_platform,
+    repo_name,
+    github_username=None,
+    github_repo_owner=None,
+    azdo_org=None,
+    azdo_project=None,
+):
     """Create an initial branch and commit files."""
     print(
         "[bold green]*** 'initialize-repo-and-commit-files' task start ***[/bold green]"
     )
-    answers = json.loads(answers_json)
     with open("token.json") as token_file:
         token = json.loads(token_file.read())["token"]
-    owner = answers.get("github_org") or answers.get("github_username")
-    if answers["lifecycle"] in ["Pre-Alpha", "Alpha", "Beta"]:
+    if lifecycle in ["Pre-Alpha", "Alpha", "Beta"]:
         first_version = "0.1.0"
     else:
         first_version = "1.0.0"
@@ -222,41 +226,36 @@ def initialize_repo_and_commit_files(c, answers_json):
     print("[cyan]Initializing git repo with 'main' branch...[/cyan]")
     c.run("git init -b main")
     print("[cyan]Adding files to commit...[/cyan]")
-    c.run("git add --all -- ':!tasks.py' ':!token.json' ':!template_copy.tar.gz'")
+    c.run('git add --all -- ":!tasks.py" ":!token.json" ":!mise.init.toml"')
     print("[cyan]Committing...[/cyan]")
-    commit_message = "git commit -m 'feat: initialize project'"
-    if answers["developer_platform"] == "GitHub":
-        commit_message += f" -m 'Release-As: {first_version}'"
-    c.run(commit_message)
+    commit_cmd = 'git commit -m "feat: initialize project"'
+    if developer_platform == "GitHub":
+        commit_cmd += f' -m "Release-As: {first_version}"'
+    c.run(commit_cmd)
     print("[cyan]Adding remote...[/cyan]")
-    if answers["developer_platform"] == "GitHub":
-        remote_url = f"https://github.com/{owner}/{answers['repo_name']}.git"
+    if developer_platform == "GitHub":
+        remote_url = f"https://github.com/{github_repo_owner}/{repo_name}.git"
         gcm_dir = f"{str(Path.home())}/.gcm/store/git/https/github.com"
-        gcm_file = f"{answers['github_username']}.credential"
+        gcm_file = f"{github_username}.credential"
         gcm_service = "https://github.com"
-        gcm_account = answers["github_username"]
-    elif answers["developer_platform"] == "Azure DevOps":
-        encoded_project = answers["azdo_project"].replace(" ", "%20")
-        remote_url = f"https://{answers['azdo_org']}@dev.azure.com/{answers['azdo_org']}/{encoded_project}/_git/{answers['repo_name']}"
-        gcm_dir = (f"{str(Path.home())}/.gcm/store/git/https/dev.azure.com/"
-                   f"{answers['azdo_org']}")
+        gcm_account = github_username
+    elif developer_platform == "Azure DevOps":
+        encoded_project = azdo_project.replace(" ", "%20")
+        remote_url = f"https://{azdo_org}@dev.azure.com/{azdo_org}/{encoded_project}/_git/{repo_name}"
+        gcm_dir = f"{str(Path.home())}/.gcm/store/git/https/dev.azure.com/{azdo_org}"
         gcm_file = "copier.credential"
-        gcm_service = f"https://dev.azure.com/{answers['azdo_org']}"
+        gcm_service = f"https://dev.azure.com/{azdo_org}"
         gcm_account = "copier"
         print("[cyan]Temporarily setting git config options for AzDO...[/cyan]")
         c.run("git config credential.useHttpPath true")
     c.run(f"git remote add origin {remote_url}")
     print("[cyan]Setting up Git credentials...[/cyan]")
     print(
-        "[cyan]"
-        "Temporarily enabling plaintext git credentials for first push..."
-        "[/cyan]"
+        "[cyan]Temporarily enabling plaintext git credentials for first push...[/cyan]"
     )
     c.run("git config credential.credentialStore plaintext")
     print(
-        "[cyan]"
-        "Creating credentials file that will be cleaned up after push..."
-        "[/cyan]"
+        "[cyan]Creating credentials file that will be cleaned up after push...[/cyan]"
     )
     Path(gcm_dir).mkdir(parents=True, exist_ok=True)
     with open(f"{gcm_dir}/{gcm_file}", "w+") as cred_file:
@@ -265,7 +264,7 @@ def initialize_repo_and_commit_files(c, answers_json):
         )
     print("[cyan]Pushing to remote...[/cyan]")
     c.run("git push -u origin --all")
-    if answers["developer_platform"] == "Azure DevOps":
+    if developer_platform == "Azure DevOps":
         print("[cyan]Unsetting git config options for AzDO...[/cyan]")
         c.run("git config --unset credential.useHttpPath")
     print("[cyan]Disabling plaintext git credentials...[/cyan]")
@@ -278,19 +277,18 @@ def initialize_repo_and_commit_files(c, answers_json):
 
 
 @task
-def create_pipelines_azdo(c, answers_json):
+def create_pipelines_azdo(c, repo_name, azdo_project, azdo_org):
     """Register pipelines for an Azure DevOps repo."""
     print("[bold green]*** 'create-pipelines-azdo' task start ***[/bold green]")
-    answers = json.loads(answers_json)
     with open("token.json") as token_file:
         token = json.loads(token_file.read())["token"]
 
     for entry in os.scandir(".azurepipelines"):
-        if entry.name.endswith(".yml"):
+        if entry.name.endswith(".yml") and not entry.name.startswith("template-"):
             pipeline_data = {
-                "name": f"{answers['repo_name']} {Path(entry.name).with_suffix('')}",
+                "name": f"[{repo_name}] {Path(entry.name).with_suffix('')}",
                 "repository": {
-                    "name": answers["repo_name"],
+                    "name": repo_name,
                     "type": "TfsGit",
                 },
                 "process": {"yamlFilename": f".azurepipelines/{entry.name}", "type": 2},
@@ -302,14 +300,14 @@ def create_pipelines_azdo(c, answers_json):
                 "type": "build",
             }
             headers = {"Content-Type": "application/json", "Accept": "application/json"}
-            encoded_project = answers["azdo_project"].replace(" ", "%20")
+            encoded_project = azdo_project.replace(" ", "%20")
             print(
                 f"[cyan]"
                 f"Creating pipeline for '.azurepipelines/{entry.name}' in "
                 "Azure DevOps...[/cyan]"
             )
             response = requests.post(
-                f"https://dev.azure.com/{answers['azdo_org']}/{encoded_project}/_apis/build/definitions?api-version=7.1-preview.7",
+                f"https://dev.azure.com/{azdo_org}/{encoded_project}/_apis/build/definitions?api-version=7.1-preview.7",
                 data=json.dumps(pipeline_data),
                 auth=("", token),
                 headers=headers,
@@ -317,6 +315,31 @@ def create_pipelines_azdo(c, answers_json):
             )
             response.raise_for_status()
     print("[bold green]*** 'create-pipelines-azdo' task end ***[/bold green]")
+
+
+@task
+def setup_mkdocs_ghpages(c, github_repo_owner, repo_name):
+    """Perform initial setup for mkdocs on GitHub."""
+    print("[bold green]*** 'setup-mkdocs' task start ***[/bold green]")
+
+    with open("token.json") as token_file:
+        token = json.loads(token_file.read())["token"]
+
+    print("[cyan]Authenticating to GitHub...[/cyan]")
+    github = githubkit.GitHub(githubkit.TokenAuthStrategy(token))
+
+    # Enable Pages
+    github.rest.repos.create_pages_site(
+        owner=github_repo_owner, repo=repo_name, data={"build_type": "workflow"}
+    )
+
+    github.rest.repos.create_or_update_environment(
+        owner=github_repo_owner,
+        repo=repo_name,
+        environment_name="github-pages",
+        data={"deployment_branch_policy": None},
+    )
+    print("[bold green]*** 'setup-mkdocs' task end ***[/bold green]")
 
 
 @task
